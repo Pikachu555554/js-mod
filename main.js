73e4d20c54adc87bb14627bdbc4864252bf44713
Pikachu555554 <291097464+Pikachu555554@users.noreply.github.com>
2026-07-26 16:04:36 -0700

    Add ultra_fire element with properties

---
+++ main.js
@@ -0,0 +1,12 @@
+elements.ultra_fire ={
+   color:"#3647df",
+   behavior: [
+      "XX|CH:fire|XX",
+      "CH:fire|XX|CH:fire",
+      "XX|DL:fire|XX"
+   ],
+   burn:1000,
+   burnInto:"explosion",
+   temp: 100000000000,
+   category: "energy",
+}
