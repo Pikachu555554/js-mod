f34e786e513ce458b61c8bf8f299e2188d6bbfdd
Pikachu555554 <291097464+Pikachu555554@users.noreply.github.com>
2026-07-26 17:27:27 -0700

    Update ultra_fire behavior to use behaviors.FIRE

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 elements.ultra_fire ={
    color:"#3647df",
-   behavior: [
-      "XX|CH:fire|XX",
-      "CH:fire|XX|CH:fire",
-      "XX|DL:fire|XX"
-   ],
+   behavior: behaviors.FIRE,
    burn:1000,
    burnInto:"explosion",
    temp: 100000000000,
